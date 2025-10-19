49d795e5d09d6ec4f7361500db151d2cd7d95deb
Yuya46086 <fuji40726@gmail.com>
2025-10-19 23:24:37 +0900

    Postモデルとテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_15_111827) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_19_141616) do
+  create_table "posts", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.text "content"
+    t.string "image_url"
+    t.date "post_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -23,4 +33,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_15_111827) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
