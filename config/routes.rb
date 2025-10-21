bd5768672d85ea772e477602bcdffd521d3529aa
Yuya46086 <fuji40726@gmail.com>
2025-10-21 20:10:05 +0900

    ルーティングの定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'quiz_attempts/new'
+  get 'quiz_attempts/create'
   devise_for :users
   root 'dashboards#show'
   resources :posts
+  resources :quiz_attempts, only: [:new, :create]
 end
