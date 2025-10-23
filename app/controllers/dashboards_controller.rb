4843db565f6a152fa8f65988ba93a8ae9bb06f39
Yuya46086 <fuji40726@gmail.com>
2025-10-23 22:08:50 +0900

    クイズ挑戦結果の表示

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,15 @@
 class DashboardsController < ApplicationController
   def show
-    
+    # 最新のクイズ挑戦結果を1件取得
+    @latest_attempt = current_user.quiz_attempts.order(created_at: :desc).first
+
+    # ユーザーがまだ回答していないクイズの数
+    user_posts_ids = current_user.posts.pluck(:id)
+    answered_quiz_ids = current_user.quiz_attempts.pluck(:daily_quiz_id)
+
+    @remaining_quiz_count = DailyQuiz
+                              .where(post_id: user_posts_ids)
+                              .where.not(id: answered_quiz_ids)
+                              .count
   end
 end
