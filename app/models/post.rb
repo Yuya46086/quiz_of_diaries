dea60c6069fa5bf6a864eb97dc378227dcaa61e8
Yuya46086 <fuji40726@gmail.com>
2025-10-26 20:10:17 +0900

    バリデーションの強化

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one :daily_quiz, dependent: :destroy
 
+  validates :post_date, presence: true
+  validates :content, presence: true
+
   accepts_nested_attributes_for :daily_quiz
 end
