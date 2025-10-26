2b0857eec413a1ed12a00b7e7b67e396633d44e0
Yuya46086 <fuji40726@gmail.com>
2025-10-26 17:38:51 +0900

    ランキングページの作成

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,4 +1,12 @@
 class RankingsController < ApplicationController
+  before_action :authenticate_user!
+
   def index
+    @user = User
+              .left_joins(:quiz_attempts)
+              .group(:id)
+              .select('users.*, SUM(COALESCE(quiz_attempts.score_awarded, 0)) AS total_score')
+              .order('total_score DESC')
+              .limit(10) # トップ10を表示
   end
 end
