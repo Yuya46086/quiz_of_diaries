1c61753964ae30407062c20ab6de30852aa78ec9
Yuya46086 <fuji40726@gmail.com>
2025-10-30 21:40:37 +0900

    コードの修正

--- app/models/daily_quiz.rb
+++ app/models/daily_quiz.rb
@@ -1,7 +1,7 @@
 class DailyQuiz < ApplicationRecord
   belongs_to :user
   belongs_to :post
-  has_many :quiz_attempts
+  has_many :quiz_attempts, dependent: :destroy
 
   validates :question_text, presence: true
   validates :correct_answer, presence: true
