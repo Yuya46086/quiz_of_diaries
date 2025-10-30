b14b8c47c346a332c12b2097527fb591fd78a8aa
Yuya46086 <fuji40726@gmail.com>
2025-10-30 20:15:14 +0900

    ユーザーネームカラムの追加と表示

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_10_24_134946) do
+ActiveRecord::Schema[7.1].define(version: 2025_10_30_111157) do
   create_table "daily_quizzes", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
@@ -54,8 +54,10 @@ ActiveRecord::Schema[7.1].define(version: 2025_10_24_134946) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "daily_quizzes", "posts"
